9bd40ca2a31507b2383e823cfd7b38fc3ca8a304
nuthan <nuthan.murarysetty@ctcteam.com>
2012-09-24 15:39:16 +0530

    view and styles and controller modified

--- app/controllers/userinfo_controller.rb
+++ app/controllers/userinfo_controller.rb
@@ -1,7 +1,7 @@
 class UserinfoController < ApplicationController
   layout "Commonlayout"
   
-  before_filter :protect,:except =>[:login,:new]
+  before_filter :protect,:except =>[:login,:register]
  
  # the below action executes every time but excepts on login and register time because it is used on new or already logged users
  # and it is executes remaining actions which is in this controller.  
@@ -15,8 +15,8 @@ class UserinfoController < ApplicationController
   end
   
   def index
-    @user=Userlogindetail.new(params[:userlogindetail])
-    currentuser=Userlogindetail.find_by_id(session[:user_id])
+    @user=Userlogindetails.new(params[:userlogindetails])
+    currentuser=Userlogindetails.find_by_id(session[:user_id])
     @currentuser=currentuser
     respond_to do |format|
       format.html
@@ -29,8 +29,8 @@ class UserinfoController < ApplicationController
     respond_to do|format|
       format.html    
     if request.post?
-      @user=Userlogindetail.new(params[:userlogindetail])
-      userinfo=Userlogindetail.find_by_useremail_and_userpassword(@user.useremail,@user.userpassword)
+      @user=Userlogindetails.new(params[:userlogindetails])
+      userinfo=Userlogindetails.find_by_useremail_and_userpassword(@user.useremail,@user.userpassword)
       if userinfo
         session[:user_id]=userinfo.id
        # flash[:notice]="User #{userinfo.username} logged in."        
@@ -46,19 +46,16 @@ class UserinfoController < ApplicationController
   end
 
 #Post Method of HTTP
-def new
-    @user=Userlogindetail.new    
+
+  def register
+    @user=Userlogindetails.new    
     respond_to do |format|
       format.html
       format.json {render json @user}
-  end
-end
-
-  def create      
+      
     if request.post?
-      @user = Userlogindetail.new(params[:userlogindetail])
-      if @user.save 
-     Registrationmail.deliver_registrationmailconfirm(@user)
+      @user = Userlogindetails.new(params[:userlogindetails])
+      if @user.save       
          #format.html { redirect_to :action=>'index'}
          #format.html { redirect_to(@user, :notice => 'User was successfully created.') }
          #session[:login]=1 
@@ -68,12 +65,12 @@ end
          return
          #format.json { render json: @user, status: :created, location: @user }
       else
-        format.html { render action: "new" }
+        format.html { render action: "register" }
         format.json { render json: @user.errors, status: :unprocessable_entity }
         #session[:login]=nil
       end
     end
- 
+  end
  end
  def logout
    session[:user_id]=nil
